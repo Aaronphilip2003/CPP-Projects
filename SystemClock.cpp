61075fd45c45331c2e90d051b35d705736d98e93
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-10-26 01:54:22 +0530

    Add files via upload

---
+++ SystemClock.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+    int h,s,m,a,err;
+    err=a=0;
+    while(err==0)
+    {
+        cout<<"Enter the hour:"<<endl;
+        cin>>h;
+        cout<<"Enter the minute:"<<endl;
+        cin>>m;
+        cout<<"Enter the seconds:"<<endl;
+        cin>>s;
+        if(h<24&&m<60&&s<60)
+        err++;
+        else
+        {
+            system("cls");
+        }
+    }
+    while(a==0)
+    {
+        system("cls");
+        cout<<h<<":"<<m<<":"<<s<<endl;
+        Sleep(1000);
+        s++;
+        if(s>59)
+        {
+            s=0;
+            m++;
+        }
+        if(m>59)
+        {
+            m=0;
+            h++;
+        }
+        if(h>24)
+        {
+            h=0;
+        }
+    }
+    return 0;
+}
